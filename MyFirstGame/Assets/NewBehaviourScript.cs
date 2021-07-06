538b55f766bc12cd15e469e4b9c61e74b743f39b
Deguriii <40736396+MinTpie30@users.noreply.github.com>
2021-07-06 12:13:04 +0900

    Update collide files

---
+++ MyFirstGame/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Start");
+    }
+    
+    void Update()
+    {
+      /*  if (Input.anyKeyDown)
+            Debug.Log("왜 눌렀니"); */
+
+        if (Input.GetButtonDown("Jump"))
+            Debug.Log("점핑!점핑!에블바리");
+    }
+
+ 
+}
