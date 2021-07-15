aef43a1257d5812da97fbd4881b0bff883038580
junho <jhshin9345@gmail.com>
2021-07-15 16:22:49 +0900

    Updated Control build files

---
+++ Control/Assets/LSController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSController : MonoBehaviour
+{
+    Rigidbody rgd;
+
+    void Start()
+    {
+        rgd = GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rgd.AddForce(Vector3.up * 100, ForceMode.Impulse);
+        }
+
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Vertical"), 0, -Input.GetAxisRaw("Horizontal"));
+
+        rgd.AddForce(vec , ForceMode.Impulse);
+    }
+}
